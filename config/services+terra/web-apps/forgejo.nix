872edf07100dd617c910a7d4f17760b37e92e565
Wietse de Vries <wietsedv@proton.me>
2026-01-03 14:22:01 +0100

    add flake update forgejo action

--- config/services+terra/web-apps/forgejo.nix
+++ config/services+terra/web-apps/forgejo.nix
@@ -11,4 +11,16 @@
   };
 
   custom.web-apps.forgejo.port = config.services.forgejo.settings.server.HTTP_PORT;
+
+  services.gitea-actions-runner = {
+    package = pkgs.forgejo-runner;
+
+    instances.${config.networking.hostName} = {
+      enable = true;
+      url = config.services.forgejo.settings.server.ROOT_URL;
+      name = config.networking.hostName;
+      tokenFile = "/var/lib/gitea-runner/token.env";
+      labels = [ "nixos:host" ];
+    };
+  };
 }
