ca2c1d4c9ac330f3bd9b917e1729415fe5eb71ff
Wietse de Vries <wietsedv@proton.me>
2026-01-05 17:59:37 +0100

    fix hyprpaper

--- config/desktop+client/+thinkpad/theme.home.nix
+++ config/desktop+client/+thinkpad/theme.home.nix
@@ -30,9 +30,9 @@
 
   services.hyprpaper = {
     enable = true;
-    settings = {
-      preload = "/etc/nixos/wallpaper.png";
-      wallpaper = ", /etc/nixos/wallpaper.png";
+    settings.wallpaper = {
+      monitor = "";
+      path = "/etc/nixos/wallpaper.png";
     };
   };
 }
