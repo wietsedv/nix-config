27018d9c41694731c95eabd2c00ceecb4e5dc763
Wietse de Vries <wietsedv@proton.me>
2026-01-05 18:03:10 +0100

    remove hyprland windowrules because it changed in 0.53

--- config/desktop+client/+thinkpad/hyprland.home.nix
+++ config/desktop+client/+thinkpad/hyprland.home.nix
@@ -107,11 +107,6 @@
         "3, horizontal, workspace"
       ];
 
-      windowrule = [
-        "suppressevent maximize, class:.*"
-        "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
-      ];
-
       bind = [
         "SUPER, Q, killactive"
         "SUPER, T, exec, theme-toggle"
