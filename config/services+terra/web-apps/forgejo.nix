a4b507fc4a1d31d311639dcc87d1ca8941e66879
Wietse de Vries <wietsedv@proton.me>
2026-02-08 14:56:59 +0100

    disable forgejo runner

--- config/services+terra/web-apps/forgejo.nix
+++ config/services+terra/web-apps/forgejo.nix
@@ -12,23 +12,23 @@
 
   custom.web-apps.forgejo.port = config.services.forgejo.settings.server.HTTP_PORT;
 
-  services.gitea-actions-runner = {
-    package = pkgs.forgejo-runner;
+  # services.gitea-actions-runner = {
+  #   package = pkgs.forgejo-runner;
 
-    instances.${config.networking.hostName} = {
-      enable = true;
-      url = config.services.forgejo.settings.server.ROOT_URL;
-      name = config.networking.hostName;
-      tokenFile = "/var/lib/gitea-runner/token.env";
+  #   instances.${config.networking.hostName} = {
+  #     enable = true;
+  #     url = config.services.forgejo.settings.server.ROOT_URL;
+  #     name = config.networking.hostName;
+  #     tokenFile = "/var/lib/gitea-runner/token.env";
 
-      labels = [ "nixos:host" ];
-      hostPackages = with pkgs; [
-        bash
-        coreutils
-        openssh
-        nodejs
-        config.nix.package
-      ];
-    };
-  };
+  #     labels = [ "nixos:host" ];
+  #     hostPackages = with pkgs; [
+  #       bash
+  #       coreutils
+  #       openssh
+  #       nodejs
+  #       config.nix.package
+  #     ];
+  #   };
+  # };
 }
