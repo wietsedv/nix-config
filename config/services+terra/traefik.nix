a814f5ca9482aa55f40c78a4cac9cfad85bcdfb0
Wietse de Vries <wietsedv@proton.me>
2026-02-13 15:57:29 +0100

    migrate traefik to new config format

--- config/services+terra/traefik.nix
+++ config/services+terra/traefik.nix
@@ -36,7 +36,7 @@ in
       enable = true;
       environmentFiles = [ "/var/lib/traefik/traefik.env" ];
 
-      staticConfigOptions = {
+      static.settings = {
         api.dashboard = true;
 
         certificatesResolvers.cloudflare.acme = {
@@ -65,57 +65,53 @@ in
             };
           };
         };
-
-        providers = lib.mkIf config.virtualisation.podman.enable {
-          docker = {
-            endpoint = "unix:///var/run/podman/podman.sock";
-            defaultRule = "Host(`{{ normalize .ContainerName }}.${domain}`)";
-            allowEmptyServices = true;
-          };
-        };
       };
 
-      dynamicConfigOptions.http = lib.mkMerge [
-        {
-          middlewares = {
-            sts-header.headers = {
-              stsSeconds = 15552000;
+      dynamic = {
+        dir = "/var/lib/traefik/dynamic";
+
+        files.web-apps.settings.http = lib.mkMerge [
+          {
+            middlewares = {
+              sts-header.headers = {
+                stsSeconds = 15552000;
+              };
             };
-          };
-          routers = {
-            traefik = {
-              rule = "Host(`traefik.${domain}`)";
-              service = "api@internal";
+            routers = {
+              traefik = {
+                rule = "Host(`traefik.${domain}`)";
+                service = "api@internal";
+              };
             };
-          };
-        }
-        {
-          routers = (
-            lib.mapAttrs (n: v: {
-              rule = "Host(`${n}.${domain}`)";
-              service = "${n}";
-            }) enabledWebApps
-          );
-          services = (
-            lib.mapAttrs (n: v: {
-              loadBalancer.servers = lib.singleton { url = "http://${v.host}:${toString v.port}"; };
-            }) enabledWebApps
-          );
-        }
-        {
-          routers = (
-            lib.mapAttrs (name: app: {
-              rule = "Host(`${name}.${domain}`)";
-              service = "${name}";
-            }) config.custom.web-apps
-          );
-          services = (
-            lib.mapAttrs (name: app: {
-              loadBalancer.servers = lib.singleton { url = "http://127.0.0.1:${toString app.port}"; };
-            }) config.custom.web-apps
-          );
-        }
-      ];
+          }
+          {
+            routers = (
+              lib.mapAttrs (n: v: {
+                rule = "Host(`${n}.${domain}`)";
+                service = "${n}";
+              }) enabledWebApps
+            );
+            services = (
+              lib.mapAttrs (n: v: {
+                loadBalancer.servers = lib.singleton { url = "http://${v.host}:${toString v.port}"; };
+              }) enabledWebApps
+            );
+          }
+          {
+            routers = (
+              lib.mapAttrs (name: app: {
+                rule = "Host(`${name}.${domain}`)";
+                service = "${name}";
+              }) config.custom.web-apps
+            );
+            services = (
+              lib.mapAttrs (name: app: {
+                loadBalancer.servers = lib.singleton { url = "http://127.0.0.1:${toString app.port}"; };
+              }) config.custom.web-apps
+            );
+          }
+        ];
+      };
     };
   };
 }
