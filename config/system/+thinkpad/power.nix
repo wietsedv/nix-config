ab3ddb353b50e18459fe3afa366c0083716499d8
Wietse de Vries <wietsedv@proton.me>
2026-03-16 11:38:30 +0100

    use auto-cpufreq

--- config/system/+thinkpad/power.nix
+++ config/system/+thinkpad/power.nix
@@ -6,41 +6,25 @@
     powertop
   ];
 
-  powerManagement = {
-    enable = true;
-    cpuFreqGovernor = "performance";
-    # powertop.enable = true;
-  };
-
-  # services.power-profiles-daemon.enable = true;
-
   services.upower.enable = true;
 
-  # services.auto-cpufreq = {
-  #   enable = true;
-  #   settings = {
-  #     charger = {
-  #        governor = "performance";
-  #        energy_performance_preference = "performance";
-  #        energy_perf_bias = "performance";
-  #        platform_profile = "performance";
-  #        # scaling_min_freq = ;
-  #        # scaling_max_freq = ;
-  #        turbo = "auto";
-  #     };
-  #     battery = {
-  #        governor = "powersave";
-  #        energy_performance_preference = "power";
-  #        energy_perf_bias = "power";
-  #        platform_profile = "low-power";
-  #        # scaling_min_freq = ;
-  #        # scaling_max_freq = ;
-  #        turbo = "auto";
-  #     };
-  #   };
-  # };
-
-  # services.thinkfan = {
-  #   enable = true;
-  # };
+  services.auto-cpufreq = {
+    enable = true;
+    settings = {
+      charger = {
+        governor = "performance";
+        energy_performance_preference = "performance";
+        energy_perf_bias = "performance";
+        platform_profile = "performance";
+        turbo = "auto";
+      };
+      battery = {
+        governor = "powersave";
+        energy_performance_preference = "balance_power";
+        energy_perf_bias = "balance_power";
+        platform_profile = "low-power";
+        turbo = "auto";
+      };
+    };
+  };
 }
