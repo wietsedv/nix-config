953efb76554d26b4692081fb8233cabdc99bc967
Wietse de Vries <wietsedv@proton.me>
2026-04-09 14:20:56 +0200

    upgrade to python 3.14

--- config/programs/+client/python.nix
+++ config/programs/+client/python.nix
@@ -1,13 +1,18 @@
 { pkgs, ... }:
 
+let
+  python = pkgs.python314;
+in
 {
   environment.variables = {
-    UV_PYTHON = "${pkgs.python3}/bin/python3";
+    UV_PYTHON = "${python}/bin/python3";
     UV_PYTHON_DOWNLOADS = "never";
   };
 
-  environment.systemPackages = with pkgs; [
-    python3
-    uv
+  environment.systemPackages = [
+    python
+    pkgs.uv
   ];
+
+  programs.nix-ld.enable = true;
 }
