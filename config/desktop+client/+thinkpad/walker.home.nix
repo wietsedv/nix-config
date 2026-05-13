289e97cb5ebe82293f2a632b6945e82a117549f9
Wietse de Vries <wietsedv@proton.me>
2026-05-13 09:25:16 +0200

    use lua generator for elephant

--- config/desktop+client/+thinkpad/walker.home.nix
+++ config/desktop+client/+thinkpad/walker.home.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
   programs.walker = {
@@ -30,37 +30,42 @@
 
   programs.elephant = {
     package = pkgs.elephant;
-    provider.menus.lua.vscode-workspaces = ''
-      Name = "vscode-workspaces"
-      NamePretty = "Visual Studio Code Workspaces"
-      Description = "Visual Studio Code Workspaces"
-      Icon = "applications-other"
-      Action = "code %VALUE%"
-      SearchName = true
-      Cache = true
-
-      function GetEntries()
-          local entries = {}
-
-          local handle = io.popen("find '/home/wietse/Projecten' -maxdepth 2 -type f -name '*.code-workspace' 2>/dev/null")
-          if handle then
-              for line in handle:lines() do
-                  local filename = line:match("([^/]+)$")
-                  if filename then
-                      table.insert(entries, {
-                          Text = filename,
-                          Subtext = "Visual Studio Code Workspace",
-                          Value = line,
-                          -- Preview = line,
-                          -- PreviewType = "file"
-                      })
+    provider.menus.lua = {
+      "vscode-workspaces" =
+        lib.generators.toLua
+          {
+            multiline = true;
+            asBindings = true;
+          }
+          {
+            Name = "vscode-workspaces";
+            NamePretty = "Visual Studio Code Workspaces";
+            Description = "Visual Studio Code Workspaces";
+            Icon = "applications-other";
+            Action = "code %VALUE%";
+            SearchName = true;
+            Cache = true;
+            GetEntries = lib.generators.mkLuaInline ''
+              function()
+                  local entries = {}
+                  local handle = io.popen("find '/home/wietse/Projecten' -maxdepth 2 -type f -name '*.code-workspace' 2>/dev/null")
+                  if handle then
+                      for line in handle:lines() do
+                          local filename = line:match("([^/]+)$")
+                          if filename then
+                              table.insert(entries, {
+                                  Text = filename,
+                                  Subtext = "Visual Studio Code Workspace",
+                                  Value = line,
+                              })
+                          end
+                      end
+                      handle:close()
                   end
+                  return entries
               end
-              handle:close()
-          end
-
-          return entries
-      end
-    '';
+            '';
+          };
+    };
   };
 }
