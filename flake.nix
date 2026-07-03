7b1e36422196374083b7902bd4725a941c28911b
Wietse de Vries <wietsedv@proton.me>
2026-07-03 13:46:42 +0200

    upgrade lanzaboote

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     };
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v1.0.0";
+      url = "github:nix-community/lanzaboote/v1.1.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
