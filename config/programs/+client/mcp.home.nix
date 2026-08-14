0628530ed69334951c2e14512045769642278494
Wietse de Vries <wietsedv@proton.me>
2026-08-14 14:36:38 +0200

    add playwright mcp

--- config/programs/+client/mcp.home.nix
+++ config/programs/+client/mcp.home.nix
@@ -9,6 +9,14 @@
         command = "${pkgs.mcp-nixos}/bin/mcp-nixos";
         args = [ ];
       };
+      "playwright-nix" = {
+        type = "stdio";
+        command = "${pkgs.playwright-test}/bin/playwright";
+        args = [
+          "mcp"
+          "--browser=chromium"
+        ];
+      };
     };
   };
 }
